e0633f7e243e1d6e84724f37529d67a290104675
randyttruong <randyrtt12@gmail.com>
2024-08-02 23:55:38 -0500

    added syntax analyzer tests

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,10 @@ int main() {
         "set \"this\" \"test\""
     };
 
-    std::cout << "set \"these\" nuts" << std::endl;
-
     for (size_t i = 0; i < testStrings.size(); i++) {
         std::string currTest = std::to_string(i);
         std::cout << "=======" << " TEST " + currTest + " " << "=======" << std::endl ;
+        std::cout << "String: " << testStrings[i] << std::endl;
         Tokenizer tokenizer = Tokenizer(testStrings[i], 0);
         tokenizer.tokenize();
         std::vector<Token> tokenStream = tokenizer.getTokenStream();
@@ -27,27 +26,7 @@ int main() {
 
         Parser parser = Parser(tokenStream);
         parser.parseTokenStream();
-
-        OperatorType type = parser.ast.getOperatorType();
-        std::string final;
-
-        switch (type) {
-            case OperatorType::GET:
-                final = "get";
-                break;
-            case OperatorType::SET:
-                final = "set";
-                break;
-            case OperatorType::DELETE:
-                final = "delete";
-                break;
-            default:
-                final = "";
-                break;
-        }
-
-        std::cout << "OPERATOR TYPE " + final << std::endl;
+        parser.ast.printOperatorType();
     }
-
     return 0;
 }
