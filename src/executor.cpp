aecb35cb6da717a13555a4c9fab4abe3e9aa7f90
randyttruong <randyrtt12@gmail.com>
2024-08-15 20:33:12 -0500

    add executor class files

---
+++ src/executor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Randy Truong on 8/15/24.
+//
+
+#include "executor.h"
---
+++ src/executor.h
@@ -0,0 +1,16 @@
+//
+// Created by Randy Truong on 8/15/24.
+//
+
+#ifndef EXECUTOR_H
+#define EXECUTOR_H
+
+
+
+class Executor {
+
+};
+
+
+
+#endif //EXECUTOR_H
