7ad8ef4826a28ce50f129c126c383ed429639eca
Randy Truong <rtruong@u.northwestern.edu>
2024-08-18 12:28:58 -0500

    documentation for semantic analyzer

--- src/analyzer.h
+++ src/analyzer.h
@@ -1,14 +1,28 @@
 //
-// Created by Randy Truong on 8/2/24.
+// analyzer.h
 //
+// Implementation of an AbstractSyntaxTree parser in C++.
+//
+// Fields:
+// - TODO
+// Getter/Setter Methods:
+// - TODO
+// Functional Methods:
+// - TODO
+//
+// Created by Randy Truong on 08/02/24.
+//
+
 
 #ifndef ANALYZER_H
 #define ANALYZER_H
 #include "ast.h"
+#include "parser.h"
 
 
 class Analyzer {
 private:
+    Parser parser;
     AbstractSyntaxTree ast;
 public:
     Analyzer();
